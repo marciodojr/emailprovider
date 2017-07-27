5f48e9535dd30244c7a063f56c282c3837fb361a
Márcio Dias <marciojr91@gmail.com>
2017-07-27 16:39:02 -0300

    Adição do PDO para conexões com o banco de dados

--- app/src/Controller/HelloController.php
+++ app/src/Controller/HelloController.php
@@ -4,10 +4,23 @@
 namespace IntecPhp\Controller;
 
 
+use IntecPhp\Model\DbHandler;
+
 class HelloController
 {
     public static function index()
     {
+
+        // $dbh = DbHandler::getInstance();
+        // $stmt = $dbh->prepare('select * from start where id in(:id1, :id2)', [':id1' => 1, ':id2' => 3]);
+
+        // if($stmt) {
+        //     while($row = $stmt->fetch()) {
+        //         print_r($row);
+        //     }
+        //     exit;
+        // }
+
         return 'Hello';
     }
 }
--- app/src/Model/DbHandler.php
+++ app/src/Model/DbHandler.php
@@ -1,6 +1,11 @@
 <?php
 
+
 namespace IntecPhp\Model;
+
+
+use PDO;
+use PDOException;
 
 /**
  * Faz a manipulação do banco de dados da aplicação
@@ -25,11 +30,18 @@ class DbHandler
             $pass = getenv('DB_PASS');
             $charset = getenv('DB_CHARSET');
 
-            $this->conn = mysqli_connect($host, $user, $pass, $db);
-            $this->conn->set_charset($charset);
+            $this->conn = new PDO(
+                'mysql:host='.$host.';dbname='.$db.';charset=' . $charset,
+                $user,
+                $pass,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_PERSISTENT => false,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+            );
         } catch (\Exception $e) {
-            Log::write($e->getMessage(), __METHOD__);
-            die("Erro na conexão com o banco de dados.");
+            die("Erro na conexão com o banco de dados: ". $e->getMessage());
         }
     }
 
@@ -41,7 +53,43 @@ class DbHandler
             self::$instance = new static();
         }
 
-        return self::$instance->conn;
+        return self::$instance;
+    }
+
+    public function beginTransaction()
+    {
+        return $this->conn->beginTransaction();
+    }
+
+    public function commit()
+    {
+        return $this->conn->commit();
+    }
+
+    public function query($sql)
+    {
+        try {
+            return $this->conn->query($sql);
+        } catch(PDOException $e) {
+            if($this->conn->inTransaction()) {
+                $this->conn->rollBack();
+            }
+            error_log($e->getMessage());
+        }
+    }
+
+    public function prepare($queryString, array $params)
+    {
+        try {
+            $sth = $this->conn->prepare($queryString);
+            $sth->execute($params);
+            return $sth;
+        } catch(PDOException $e) {
+            if($this->conn->inTransaction()) {
+                $this->conn->rollBack();
+            }
+            error_log($e->getMessage());
+        }
     }
 
     private function __clone()
